cbcfd09f2b0ec81040a66d9c5cd5c780f6a6a1c3
Josias Iquabius <iquabius@gmail.com>
2018-03-11 16:05:39 -0400

    Add docker commands for cleaning up

--- config.fish
+++ config.fish
@@ -3,3 +3,19 @@ status --is-login; and status --is-interactive; and exec byobu-launcher
 set PATH $HOME "/bin" $PATH
 
 set -g theme_display_virtualenv no
+
+# https://www.calazan.com/docker-cleanup-commands/
+
+# Kill all running containers.
+alias dockerkillall='docker kill (docker ps -q)'
+
+# Delete all stopped containers.
+alias dockercleanc='printf "\n>>> Deleting stopped containers\n\n" & docker rm (docker ps -a -q)'
+
+# Delete all untagged images.
+alias dockercleani='printf "\n>>> Deleting untagged images\n\n" & docker rmi (docker images -q -f dangling=true)'
+
+alias dockercleanv='docker volume ls -qf dangling=true | xargs -r docker volume rm'
+
+# Delete all stopped containers and untagged images.
+alias dockerclean='dockercleanc; and dockercleani; or or echo "Docker cleaning failed"'
