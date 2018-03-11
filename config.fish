ca5fff325758dcec255cba8aca7d99868512095a
Josias Iquabius <iquabius@gmail.com>
2018-03-11 16:07:11 -0400

    Add ~/.local/bin to the PATH

--- config.fish
+++ config.fish
@@ -1,6 +1,6 @@
 status --is-login; and status --is-interactive; and exec byobu-launcher
 
-set PATH $HOME "/bin" $PATH
+set PATH "$HOME/bin" "$HOME/.local/bin" $PATH
 
 set -g theme_display_virtualenv no
 
