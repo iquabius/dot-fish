b89c7da661702e190ef9d37d600e651ca63b2d06
Josias Iquabius <iquabius@gmail.com>
2018-03-12 10:17:55 -0400

    Add Tex Live env vars to PATH

--- config.fish
+++ config.fish
@@ -2,4 +2,9 @@ status --is-login; and status --is-interactive; and exec byobu-launcher
 
 set PATH $HOME "/bin" $PATH
 
+# TeX Live installation
+set MANPATH "/usr/local/texlive/2017/texmf-dist/doc/man"
+set INFOPATH "/usr/local/texlive/2017/texmf-dist/doc/info"
+set PATH "/usr/local/texlive/2017/bin/x86_64-linux" $PATH
+
 set -g theme_display_virtualenv no
